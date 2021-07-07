fa374b8f8abab421899ae9c91ae00c4a75937e56
Kushagra Sharma <sharmakushagra3125@gmail.com>
2021-07-07 23:57:01 +0530

    Initial Commit

---
+++ FireChat/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FireChat
+//
+//  Created by Kushagra Sharma on 07/07/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FireChat/FireChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  FireChatApp.swift
+//  FireChat
+//
+//  Created by Kushagra Sharma on 07/07/21.
+//
+
+import SwiftUI
+
+@main
+struct FireChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
